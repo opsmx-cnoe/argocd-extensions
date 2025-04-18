d0913c24e8b755d93ed0e45c80c2e51cd62c6e82
Utkarsh Shukla <utkarsh.shukla@opsmx.io>
2025-04-18 16:09:36 +0530

    added form to

--- resources/extension-topmenu.js
+++ resources/extension-topmenu.js
@@ -23,7 +23,7 @@
           {
             method: 'PATCH',
             headers: {
-              'Content-Type': 'application/json-patch+json',
+              'Content-Type': 'application/json',
             },
             body: JSON.stringify(patch),
           }
